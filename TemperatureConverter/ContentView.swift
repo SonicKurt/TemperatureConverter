5d2ee650ce2cb5dee3045de9ac56d45119c7b91f
Kurt Campbell <kurtcampbell13@gmail.com>
2020-07-22 08:02:23 -0400

    Initial Commit

---
+++ TemperatureConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureConverter
+//
+//  Created by Kurt Campbell on 7/22/20.
+//  Copyright © 2020 Kurt Campbell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
